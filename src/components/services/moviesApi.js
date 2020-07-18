734be97317d951b22ce3bd0f62e1653c92028058
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-07-18 18:14:57 +0300

    fetch in moviesApi

--- src/components/services/moviesApi.js
+++ src/components/services/moviesApi.js
@@ -2,4 +2,9 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://api.themoviedb.org/3/";
 
+export const fetchMovies = () => {
+    const trendingParams = `trending/movie/day?api_key=${process.env.REACT_APP_API_KEY}`;
+  return axios.get(trendingParams).then(response => response.data);
+}
+
  
