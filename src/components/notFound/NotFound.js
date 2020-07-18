352f587420d18d2d4a10a1d3ebed823895d9dbc4
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-07-18 19:51:13 +0300

    NotFound (conponent)

---
+++ src/components/notFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => (
+  <h1>
+    Sorry, page not found
+    <span role="img" aria-label="emoji">
+      &#128584;
+    </span>
+  </h1>
+);
+
+export default NotFound;
